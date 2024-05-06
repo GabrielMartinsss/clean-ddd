1ef946ed4fc15e3e1494da84e21af91ec82d3d05
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-05-06 10:36:53 -0300

    feat: add delete-question and get-quest-by-slug use-cases

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.test.ts
@@ -0,0 +1,30 @@
+import { InMemoryQuestionRepository } from 'test/repositories/in-memory-question-repository'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
+import { makeQuestion } from 'test/factories/make-question'
+
+let inMemoryQuestionRepository: InMemoryQuestionRepository
+// system under test
+let sut: GetQuestionBySlugUseCase
+
+describe('Get Question by Slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionRepository = new InMemoryQuestionRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionRepository)
+  })
+
+  it('should be able to get a question by slug', async () => {
+    const newQuestion = makeQuestion({
+      slug: Slug.create('example-question'),
+    })
+
+    inMemoryQuestionRepository.create(newQuestion)
+
+    const { question } = await sut.execute({
+      slug: 'example-question',
+    })
+
+    expect(question.id).toBeTruthy()
+    expect(question.title).toEqual(newQuestion.title)
+  })
+})
