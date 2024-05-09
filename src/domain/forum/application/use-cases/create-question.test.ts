a8b5505a6d80d2b569c7bb7c5c282fd83f17f618
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-05-09 07:13:30 -0300

    feat: create Aggregate Root and add it on question

--- src/domain/forum/application/use-cases/create-question.test.ts
+++ src/domain/forum/application/use-cases/create-question.test.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { CreateQuestionUseCase } from './create-question'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 
@@ -16,9 +17,15 @@ describe('Create Question', () => {
       authorId: '1',
       title: 'New question',
       content: 'content question',
+      attachmentsId: ['1', '2'],
     })
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value?.question)
+    expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2)
+    expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityId('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityId('2') }),
+    ])
   })
 })
