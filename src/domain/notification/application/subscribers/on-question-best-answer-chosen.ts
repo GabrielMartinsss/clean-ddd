20f7a95ef4c4ea5ac3aea509ef8df5b91840902a
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-05-14 10:44:28 -0300

    feat: add notification when a question has new best answer chosen

---
+++ src/domain/notification/application/subscribers/on-question-best-answer-chosen.ts
@@ -0,0 +1,41 @@
+import { DomainEvents } from '@/core/events/domain-events'
+import { EventHandler } from '@/core/events/event-handler'
+import { SendNotificationUseCase } from '../use-cases/send-notification'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { QuestionBestAnswerChosenEvent } from '@/domain/forum/enterprise/events/question-best-answer-chosen-event'
+
+export class OnQuestionBestAnswerChosen implements EventHandler {
+  constructor(
+    private answersRepository: AnswersRepository,
+    private sendNotification: SendNotificationUseCase,
+  ) {
+    this.setupSubscriptions()
+  }
+
+  setupSubscriptions(): void {
+    DomainEvents.register(
+      // .bind(this) ensure that "this" refers OnQuestionBestAnswerChosen because this function is called in a method on DomainEvents making this refers DomainEvents
+      this.sendQuestionBestAnswerNotification.bind(this),
+      QuestionBestAnswerChosenEvent.name,
+    )
+  }
+
+  private async sendQuestionBestAnswerNotification({
+    question,
+    bestAnswerId,
+  }: QuestionBestAnswerChosenEvent) {
+    const answer = await this.answersRepository.findById(
+      bestAnswerId.toString(),
+    )
+
+    if (answer) {
+      await this.sendNotification.execute({
+        recipientId: answer.authorId.toString(),
+        title: 'Your answer was chosen as the best answer!',
+        content: `The answer that you sended in 
+          ${question.title.substring(0, 20).concat('...')} 
+          was chosen by author as the best answer.`,
+      })
+    }
+  }
+}
