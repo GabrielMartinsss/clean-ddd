d615368b6a197791b0f3d5cd90cf406636e30562
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-05-14 09:20:48 -0300

    feat: listening a domain event and sending a notification on subscribe

---
+++ src/domain/notification/application/subscribers/on-answer-created.ts
@@ -0,0 +1,36 @@
+import { DomainEvents } from '@/core/events/domain-events'
+import { EventHandler } from '@/core/events/event-handler'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { AnswerCreatedEvent } from '@/domain/forum/enterprise/events/answer-created-event'
+import { SendNotificationUseCase } from '../use-cases/send-notification'
+
+export class OnAnswerCreated implements EventHandler {
+  constructor(
+    private questionRepository: QuestionsRepository,
+    private sendNotification: SendNotificationUseCase,
+  ) {
+    this.setupSubscriptions()
+  }
+
+  setupSubscriptions(): void {
+    DomainEvents.register(
+      // .bind(this) ensure that "this" refers OnAnswerCreated because this function is called in a method on DomainEvents making this refers DomainEvents
+      this.sendNewAnswerNotification.bind(this),
+      AnswerCreatedEvent.name,
+    )
+  }
+
+  private async sendNewAnswerNotification({ answer }: AnswerCreatedEvent) {
+    const question = await this.questionRepository.findById(
+      answer.questionId.toString(),
+    )
+
+    if (question) {
+      await this.sendNotification.execute({
+        recipientId: question.authorId.toString(),
+        title: `New answer in: ${question.title.substring(0, 40).concat('...')}`,
+        content: question.excerpt,
+      })
+    }
+  }
+}
